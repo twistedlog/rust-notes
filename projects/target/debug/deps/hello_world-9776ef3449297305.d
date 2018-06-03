af7608e4b06ebbbdfd238c2a3d8087e77a924377
twistedlog <sahil.chug@fireeye.com>
2018-06-03 10:21:38 +0530

    Cargo build

---
+++ projects/target/debug/deps/hello_world-9776ef3449297305.d
@@ -0,0 +1,5 @@
+/Users/sahil.chug/Workspace/private/rust-workshop/projects/target/debug/deps/hello_world-9776ef3449297305: src/main.rs
+
+/Users/sahil.chug/Workspace/private/rust-workshop/projects/target/debug/deps/hello_world-9776ef3449297305.d: src/main.rs
+
+src/main.rs:
---
+++ projects/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/sahil.chug/Workspace/private/rust-workshop/projects/target/debug/hello_world.dSYM: /Users/sahil.chug/Workspace/private/rust-workshop/projects/src/main.rs
